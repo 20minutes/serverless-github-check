f341ca4184c52e9af32b1bcd5a8654c98e745a1f
Jeremy Benoist <jeremy.benoist@gmail.com>
2023-09-28 19:43:35 +0200

    Use classes to better handle tests
    Because of recent changes from the Octokit library (relying on Node internal fetch), we can't use `nock` anymore during tests. We must now pass a custom `fetch` to the Octokit class to mock request.
    To do that, we can no longer only use `handler` with all the code inside it, but use classes. It's also more clean :)

---
+++ functions/classes/FixupHandler.js
@@ -0,0 +1,61 @@
+import { Handler } from './Handler'
+
+export class FixupHandler extends Handler {
+  constructor(githubToken, namespace = '', fetch = null) {
+    super(githubToken, fetch)
+
+    this.namespace = namespace
+  }
+
+  async handle(body, callback) {
+    let response = this.validateEvent(body)
+
+    if (response !== true) {
+      return callback(null, response)
+    }
+
+    console.log(`Working on repo ${body.repository.full_name} for PR #${body.pull_request.number}`)
+
+    const payload = {
+      success: {
+        state: 'success',
+        description: 'No fixup commits in history',
+        context: `${this.namespace} - Fixup check`,
+      },
+      failure: {
+        state: 'failure',
+        description: 'Fixup commits in history, please squash them!',
+        context: `${this.namespace} - Fixup check`,
+      },
+    }
+
+    const compare = await this.githubClient.rest.repos.compareCommitsWithBasehead({
+      owner: body.repository.owner.login,
+      repo: body.repository.name,
+      basehead: `${body.pull_request.base.sha}...${body.pull_request.head.sha}`,
+    })
+
+    // loop through PR labels to see if we found one which should block the PR
+    const validation = compare.data.commits.every(({ commit, parents }) => {
+      const isMerge = parents && parents.length > 1
+
+      if (isMerge) {
+        return true
+      }
+
+      const match = /^fixup! .*$/im.exec(commit.message)
+
+      if (match === null) {
+        return true
+      }
+
+      console.log(`Fixup commit found: "${commit.message}"`)
+
+      return false
+    })
+
+    response = await this.updateStatus(body, validation ? payload.success : payload.failure)
+
+    return callback(null, response)
+  }
+}
---
+++ functions/classes/LabelHandler.js
@@ -0,0 +1,72 @@
+import { Handler } from './Handler'
+
+export class LabelHandler extends Handler {
+  constructor(githubToken, namespace = '', blockLabels = '', fetch = null) {
+    super(githubToken, fetch)
+
+    this.namespace = namespace
+    this.blockLabels = blockLabels
+      // convert to array
+      .split(',')
+      // trim each value
+      .map((label) => label.trim())
+      // remove empty value
+      .filter((label) => label.trim().length)
+  }
+
+  async handle(body, callback) {
+    let response = this.validateEvent(body)
+
+    if (response !== true) {
+      return callback(null, response)
+    }
+
+    console.log(`Working on repo ${body.repository.full_name} for PR #${body.pull_request.number}`)
+
+    const payload = {
+      success: {
+        state: 'success',
+        description: 'Label validation passed',
+        context: `${this.namespace} - Label validation`,
+      },
+      failure: {
+        state: 'failure',
+        description: 'Label validation failed',
+        context: `${this.namespace} - Label validation`,
+      },
+    }
+
+    // no block level defined: we allow the PR
+    if (this.blockLabels.length === 0) {
+      response = await this.updateStatus(body, payload.success)
+
+      console.log('Success: no blocked labels defined')
+
+      return callback(null, response)
+    }
+
+    // no labels defined in the PR: we block the PR
+    if (body.pull_request.labels.length === 0) {
+      response = await this.updateStatus(body, payload.failure)
+
+      console.log('Fail: no labels defined in the PR')
+
+      return callback(null, response)
+    }
+
+    // loop through PR labels to see if we found one which should block the PR
+    const validation = body.pull_request.labels.every(({ name }) => {
+      if (this.blockLabels.find((blockLabel) => blockLabel === name)) {
+        console.log('Fail: at least one blocked label found')
+
+        return false
+      }
+
+      return true
+    })
+
+    response = await this.updateStatus(body, validation ? payload.success : payload.failure)
+
+    return callback(null, response)
+  }
+}
